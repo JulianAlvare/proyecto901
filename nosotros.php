58451acc14f81d8139de49032d463d58c74e6478
kevinchis <dsm20190078.kramirez@alumnos.utsv.edu.mx>
2024-08-17 21:40:47 -0600

    cambiocont ynosotros

---
+++ nosotros.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sobre Nosotros</title>
+
+    <!-- load stylesheets -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">  <!-- Google web font "Open Sans" -->
+    <link rel="stylesheet" href="font-awesome-4.5.0/css/font-awesome.min.css">                <!-- Font Awesome -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">                                      <!-- Bootstrap style -->
+    <link rel="stylesheet" href="css/magnific-popup.css">                                     <!-- Magnific popup style -->
+    <link rel="stylesheet" href="css/templatemo-style.css">                                   <!-- Templatemo style -->
+</head>
+
+<body>
+    <!-- Header -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="tm-site-name">
+                    <h1 class="text-uppercase tm-site-name-h1">Sobre Nosotros</h1>
+                    <h2 class="tm-site-name-h2">Conoce nuestra historia</h2>    
+                </div>
+            </div>
+        </div>                
+        
+        <!-- Navbar -->
+        <?php include 'navbar.php'; ?>
+
+        <!-- Content -->
+        <div class="row tm-row-margin-b tm-content-boxes-row">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+                <div class="tm-content-box">
+                    <i class="fa fa-4x fa-briefcase tm-content-icon"></i>
+                    <h2 class="tm-content-title">Nuestra Misión</h2>
+                    <p>Proveer las mejores propiedades con un enfoque en calidad, accesibilidad, y satisfacción del cliente.</p>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+                <div class="tm-content-box">
+                    <i class="fa fa-4x fa-users tm-content-icon"></i>
+                    <h2 class="tm-content-title">Nuestro Equipo</h2>
+                    <p>Contamos con un equipo de profesionales altamente calificados para ayudarte a encontrar tu hogar ideal.</p>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+                <div class="tm-content-box">
+                    <i class="fa fa-4x fa-trophy tm-content-icon"></i>
+                    <h2 class="tm-content-title">Nuestra Historia</h2>
+                    <p>Con más de 20 años de experiencia en el mercado inmobiliario, somos líderes en la venta de propiedades.</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Additional Information -->
+        <div class="row tm-row-margin-b tm-content-boxes-row">
+            <div class="col-12">
+                <div class="tm-content-box">
+                    <h2 class="tm-content-title">Nuestro Compromiso</h2>
+                    <p>Nos comprometemos a ofrecer un servicio de calidad, apoyando a nuestros clientes en cada paso del proceso de compra.</p>
+                </div>
+            </div>
+        </div>
+        
+    </div> <!-- container-fluid -->
+    
+    <!-- load JS files -->
+    <script src="js/jquery-1.11.3.min.js"></script>             <!-- jQuery (https://jquery.com/download/) -->
+    <script src="js/popper.min.js"></script>                    <!-- https://popper.js.org/ -->
+    <script src="js/bootstrap.min.js"></script>                 <!-- https://getbootstrap.com/ -->
+    <script src="js/jquery.magnific-popup.min.js"></script>     <!-- http://dimsemenov.com/plugins/magnific-popup/ -->
+    <script src="js/templatemo-script.js"></script>             <!-- Templatemo script -->
+</body>
+</html>
