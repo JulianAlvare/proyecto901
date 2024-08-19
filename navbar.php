d703f1a6804c89dc789fad21a889b5f1ebd4298d
IrvingRobles <dsm21190235.irobles@alumnos.utsv.edu.mx>
2024-08-19 16:09:32 -0600

    navbar

--- navbar.php
+++ navbar.php
@@ -26,9 +26,7 @@
                         <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 nav-item">
                             <a class="nav-link" href="contacto.php">Contacto</a>
                         </div>
-                        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 nav-item tm-dummy-nav-item">
-                            <div class="nav-link">&nbsp;</div>
-                        </div>
+                        
                     </div>           
 
                     <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 tm-pad-0 tm-nav-item-pair">
