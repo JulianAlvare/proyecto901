93df242a38625a12b36af8bf0b1bed785850ff7b
JulianAlvare <ramirezalvarezjulian202@gmail.com>
2024-08-19 17:02:52 -0600

    comit2

---
+++ Alexis.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Contacto - Venta de Casas</title>
+
+    <!-- load stylesheets -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">  
+    <link rel="stylesheet" href="font-awesome-4.5.0/css/font-awesome.min.css">                
+    <link rel="stylesheet" href="css/bootstrap.min.css">                                      
+    <link rel="stylesheet" href="css/magnific-popup.css">                                     
+    <link rel="stylesheet" href="css/templatemo-style.css">                                   
+</head>
+
+<body>
+    <div class="container-fluid">
+        <!-- Navbar -->
+        <?php include 'navbar.php'; ?><!-- row -->
+
+        <div class="row tm-row-margin-b tm-content-boxes-row">
+            <div class="col-xs-12 col-sm-12 col-md-8 col-lg-6 col-xl-6 mx-auto">
+                <div class="tm-content-box">
+                    <h3 class="tm-content-title">Alexis Jair Ramirez Torres</h3>
+                    <p>Teleefono:</p>
+                    <p>9211678891</p>
+                    <p>Correo:</p>
+                    <p>Torresalexis083@gmail.com</p>
+                    <p>Experoencia Laboral:</p>
+                    <p>Auxiliar de recurssos humanos</p>
+                    <p>atencion a clientes</p>
+
+                </div>
+            </div>
+        </div>
+        
+    </div> <!-- container-fluid -->
+    
+    <!-- load JS files -->
+    <script src="js/jquery-1.11.3.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/templatemo-script.js"></script>
+</body>
+</html>
--- navbar.php
+++ navbar.php
@@ -36,6 +36,9 @@
                         <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 nav-item">
                             <a class="nav-link" href="kevin.php">Kevin Ramirez</a>
                         </div>
+                        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 nav-item">
+                            <a class="nav-link" href="kevin.php">Alexis Torres</a>
+                        </div>
                     </div> 
                 </div>    
             </div>                    
