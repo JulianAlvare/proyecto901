e206647e2f72b63bf97986b64295312e3479297f
IrvingRobles <dsm21190235.irobles@alumnos.utsv.edu.mx>
2024-08-19 16:06:39 -0600

    EXAMEN COMMIT

--- navbar.php
+++ navbar.php
@@ -29,8 +29,16 @@
                         <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 nav-item tm-dummy-nav-item">
                             <div class="nav-link">&nbsp;</div>
                         </div>
-                    </div>                            
-                    
+                    </div>           
+
+                    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 tm-pad-0 tm-nav-item-pair">
+                        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 nav-item">
+                            <a class="nav-link" href="irving.php">Irving Robles</a>
+                        </div>
+                        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 nav-item tm-dummy-nav-item">
+                            <div class="nav-link">&nbsp;</div>
+                        </div>
+                    </div> 
                 </div>    
             </div>                    
         </nav>     
